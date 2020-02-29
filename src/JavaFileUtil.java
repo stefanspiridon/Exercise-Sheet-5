06504cf39bc3a0d02665eff9a6631f13e1510ba4
ss8u19 <ss8u19@soton.ac.uk>
2020-02-29 22:45:46 +0000

    backup

--- src/JavaFileUtil.java
+++ src/JavaFileUtil.java
@@ -1,11 +1,25 @@
 import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io.ConcatenateJavaFiles;
 
-import java.io.IOException;
+import java.io.*;
 
 public class JavaFileUtil implements ConcatenateJavaFiles {
 
     @Override
     public void concatenateJavaFiles(String s, String s1) throws IOException {
-        
+        File file = new File(s);
+        File[] dir = file.listFiles();
+
+        OutputStream out = new FileOutputStream(s1);
+        byte[] buf = new byte[2];
+        for (File file1 : dir){
+            if(file1.isFile()){
+                InputStream in = new FileInputStream(file1);
+                int b=0;
+                while( (b=in.read(buf)) >= 0)
+                    out.write(buf, 0, b);
+                in.close();
+            }
+            out.close();
+        }
     }
 }
